5c774b2cd08ee430559716099f672a968485901e
Britany <britany.rodriguez.rojas@est.una.ac.cr>
2026-06-06 00:04:50 -0600

    Agregue atributo color

--- src/domain/Botella.java
+++ src/domain/Botella.java
@@ -3,10 +3,21 @@ package domain;
 public class Botella {
 	
 	private String color;
+	private String flavor; 
 	
-	public Botella(String color) {
+	
+	public Botella(String color, String flavor) {
 		super();
 		this.color = color;
+		this.flavor = flavor;
+	}
+
+	public String getFlavor() {
+		return flavor;
+	}
+
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
 	}
 
 	public String getColor() {
