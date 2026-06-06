7528f2844da0be35e066ab0b9f4eeff09b6310ee
Britany <britany.rodriguez.rojas@est.una.ac.cr>
2026-06-05 23:57:52 -0600

    Agregue la clase botella al paquete domain

---
+++ src/domain/Botella.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Botella {
+	
+	private String color;
+	
+	public Botella(String color) {
+		super();
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	
+
+}
